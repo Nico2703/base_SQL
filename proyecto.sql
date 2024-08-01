52a0e0ab7fdb648f5a49832744b54c1c39d34050
Nicolás Ferrand <nico_ferrand@hotmail.com>
2024-08-01 13:01:01 -0300

    Correcciones - Entrega 2

--- proyecto.sql
+++ proyecto.sql
@@ -2,17 +2,17 @@ USE elevadores_model;
 
 CREATE TABLE Clientes(
 	ID_cliente INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Denominacion VARCHAR (30),
+    Denominacion VARCHAR (60),
     Tel_cliente VARCHAR (30),
     Mail_cliente VARCHAR (30)
 );
 
 CREATE TABLE Edificios(
 	ID_edificio INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Nombre_edif VARCHAR (30),
-    Direccion VARCHAR (30),
+    Nombre_edif VARCHAR (60),
+    Direccion VARCHAR (60),
     Tel_edif VARCHAR (30),
-    Encargado VARCHAR (30),
+    Encargado VARCHAR (60),
     ID_cliente INT NOT NULL,
     FOREIGN KEY (ID_cliente) REFERENCES Clientes(ID_cliente)
 );
@@ -24,21 +24,9 @@ CREATE TABLE Elevadores(
     Paradas INT NOT NULL,
     Carga INT NOT NULL,
     Velocidad INT NOT NULL,
-    Proyecto VARCHAR (30),
+    Proyecto VARCHAR (60),
     ID_edificio INT NOT NULL,
     FOREIGN KEY (ID_edificio) REFERENCES Edificios(ID_edificio)
-);
-
-CREATE TABLE Reclamos(
-	ID_reclamo INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Falla VARCHAR (100),
-    Fecha DATE,
-    ID_edificio INT NOT NULL,
-    ID_elevador INT NOT NULL,
-    Legajo_tec INT NOT NULL,
-    FOREIGN KEY (ID_edificio) REFERENCES Edificios(ID_edificio),
-    FOREIGN KEY (ID_elevador) REFERENCES Elevadores(ID_edificio),
-    FOREIGN KEY (Legajo_tec) REFERENCES Tecnicos(Legajo_tec)
 );
 
 CREATE TABLE Tecnicos(
@@ -49,11 +37,23 @@ CREATE TABLE Tecnicos(
     Antiguedad INT NOT NULL
 );
 
+CREATE TABLE Reclamos(
+	ID_reclamo INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Falla VARCHAR (300),
+    Fecha DATE,
+    ID_edificio INT NOT NULL,
+    ID_elevador INT NOT NULL,
+    Legajo_tec INT NOT NULL,
+    FOREIGN KEY (ID_edificio) REFERENCES Edificios(ID_edificio),
+    FOREIGN KEY (ID_elevador) REFERENCES Elevadores(ID_elevador),
+    FOREIGN KEY (Legajo_tec) REFERENCES Tecnicos(Legajo_tec)
+);
+
 CREATE TABLE Reparaciones(
 	ID_reparacion INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Detalle VARCHAR (100),
-	Costo INT NOT NULL,
-    Plazo INT NOT NULL,
+    Detalle VARCHAR (300),
+	Costo FLOAT NOT NULL,
+    Plazos INT NOT NULL,
     ID_reclamo INT NOT NULL,
     ID_cliente INT NOT NULL,
     FOREIGN KEY (ID_reclamo) REFERENCES Reclamos(ID_reclamo),
