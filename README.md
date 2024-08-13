024c28b27f59d1b6e0640f1c03ae7797166df4d3
Nicolás Ferrand <nico_ferrand@hotmail.com>
2024-08-12 21:08:55 -0300

    Segunda entrega

--- README.md
+++ README.md
@@ -18,3 +18,23 @@ Alumno: Ferrand Nicolás
 
 ## Diagrama de tablas 
 <img style="width: 75%;" src="tablas.png"/>
+
+## Vistas
+- reclamos_por_tec: ordena el listado de reclamos según el técnico encargado.
+- edificios_ordenados: ordena el listado de edificios alfabéticamente.
+- elevadores_Mitsu: muestra del listado de elevadores solo aquellos de marca Mitsubishi y ordenados por ID.
+- reparaciones_urgencia: muestra el listado de reparaciones ordenado por el plazo de finalización desde la más urgente.
+
+## Funciones
+- total_deuda_cliente: recibe el ID_cliente y devuelve el costo total adeudado por reparaciones.
+- total_reclamos_edificio: recibe el ID_edificio y devuelve la cantidad de reclamos recibidos de ese edificio.
+
+## Stored Procedures
+- ordenar_reclamos: devuelve la lista ordenada de reclamos según el parámetro que recibe.
+- porcentaje_reclamos_edificio: recibe el ID_edificio y calcula que porcentaje de reclamos de este edificio existieron sobre el total de los reclamos recibidos.
+
+## Triggers
+- Se crea una tabla log_tecnicos que almacena los cambios en la tabla tecnicos, ya sea por:
+-- INSERCION: tr_insert_tecnicos.
+-- UPDATE: tr_update_tecnicos.
+-- DELETE: tr_delete_tecnicos.
--- proyecto.sql
+++ proyecto.sql
@@ -1,5 +1,6 @@
 USE elevadores_model;
 
+-- Tables
 CREATE TABLE Clientes(
 	ID_cliente INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     Denominacion VARCHAR (60),
@@ -59,3 +60,122 @@ CREATE TABLE Reparaciones(
     FOREIGN KEY (ID_reclamo) REFERENCES Reclamos(ID_reclamo),
     FOREIGN KEY (ID_cliente) REFERENCES Clientes(ID_cliente)
 );
+
+-- Views
+CREATE OR REPLACE VIEW reclamos_por_tec AS
+(SELECT Legajo_tec, COUNT(*) FROM Reclamos GROUP BY Legajo_tec ORDER BY COUNT(*) DESC);
+SELECT * FROM reclamos_por_tec;
+
+CREATE OR REPLACE VIEW edificios_ordenados AS (SELECT ID_edificio, ID_cliente, Nombre_edif, Direccion FROM Edificios ORDER BY Nombre_edif ASC);
+SELECT * FROM edificios_ordenados;
+
+CREATE OR REPLACE VIEW elevadores_Mitsu AS (SELECT ID_elevador, ID_edificio, Modelo, Paradas, Carga, Velocidad FROM Elevadores 
+WHERE Marca = "Mitsubishi" ORDER BY ID_elevador ASC);
+SELECT * FROM elevadores_Mitsu;
+
+CREATE OR REPLACE VIEW reparaciones_urgencia AS (SELECT ID_reparacion, ID_cliente, ID_reclamo, Detalle, Plazos FROM Reparaciones ORDER BY Plazos ASC);
+SELECT * FROM reparaciones_urgencia;
+DROP FUNCTION IF EXISTS total_deuda_cliente;
+-- Functions
+DELIMITER //
+CREATE FUNCTION total_deuda_cliente (Busca INT) RETURNS VARCHAR(20) DETERMINISTIC
+BEGIN
+DECLARE total FLOAT;
+DECLARE resultado VARCHAR(20);
+SET total = (SELECT SUM(Costo) FROM Reparaciones WHERE ID_cliente = Busca);
+SET resultado = CONCAT('$', FORMAT(total,2));
+RETURN resultado;
+END; //
+DELIMITER ;
+SELECT total_deuda_cliente(1) AS Deuda_cliente;
+
+DELIMITER //
+CREATE FUNCTION total_recalmos_edificio (Busca INT) RETURNS FLOAT DETERMINISTIC
+BEGIN
+DECLARE total FLOAT;
+SET total = (SELECT COUNT(ID_reclamo) FROM Reclamos WHERE ID_edificio = Busca);
+RETURN total;
+END;
+//
+DELIMITER ;
+SELECT total_recalmos_edificio(14);
+
+-- Stored Procedures
+DELIMITER //
+CREATE PROCEDURE `ordenar_reclamos`(IN campo CHAR(20))
+BEGIN
+	IF campo <> '' THEN
+		SET @orden_por = CONCAT('ORDER BY ', campo);
+	ELSE
+		SET @orden_por = '';
+	END IF;
+    SET @clausula = CONCAT('SELECT * FROM elevadores_model.reclamos ', @orden_por);
+    PREPARE accion FROM @clausula;
+    EXECUTE accion;
+    DEALLOCATE PREPARE accion;
+END //
+DELIMITER ;
+CALL ordenar_reclamos('ID_edificio');
+
+DELIMITER //
+CREATE PROCEDURE `porcentaje_reclamos_edificio`(IN campo CHAR(20))
+BEGIN
+	DECLARE total INT;
+    DECLARE suma INT;
+    DECLARE porcentaje DECIMAL(5,2);
+    SELECT COUNT(*) INTO total FROM Reclamos;
+	SELECT COUNT(*) INTO suma FROM Reclamos WHERE ID_edificio = campo;
+    IF total = 0 THEN
+		SET porcentaje = 0.00;
+	ELSE
+		SET porcentaje = suma * 100.0 / total;
+	END IF;
+    SELECT campo AS ID_edificio, suma AS Total_edificio, total AS Total_reclamos, CONCAT(FORMAT(porcentaje, 2), '%') AS Porcentaje;
+END //
+DELIMITER ;
+CALL porcentaje_reclamos_edificio('25');
+
+-- Triggers
+CREATE TABLE log_tecnicos(
+	ID INT AUTO_INCREMENT PRIMARY KEY,
+	Accion VARCHAR(20),
+	Legajo_tec INT NOT NULL,
+    Nombre_tec VARCHAR (30),
+    Apellido_tec VARCHAR(30),
+    Categoria VARCHAR(30),
+    Antiguedad INT NOT NULL,
+    Fecha DATE,
+    Hora TIME,
+    Usuario VARCHAR(20)
+);
+
+CREATE TRIGGER `tr_insert_tecnicos` 
+AFTER INSERT ON `tecnicos` 
+FOR EACH ROW 
+INSERT INTO `log_tecnicos` (Accion, Legajo_tec, Nombre_tec, Apellido_tec, Categoria, Antiguedad, Fecha, Hora, Usuario) 
+VALUES ('INSERT', NEW.Legajo_tec, NEW.Nombre_tec, NEW.Apellido_tec, NEW.Categoria, NEW.Antiguedad, NOW(), CURTIME(), CURRENT_USER());
+
+CREATE TRIGGER `tr_update_tecnicos` 
+BEFORE UPDATE ON `tecnicos` 
+FOR EACH ROW 
+INSERT INTO `log_tecnicos` (Accion, Legajo_tec, Nombre_tec, Apellido_tec, Categoria, Antiguedad, Fecha, Hora, Usuario) 
+VALUES ('UPDATE', OLD.Legajo_tec, NEW.Nombre_tec, NEW.Apellido_tec, NEW.Categoria, NEW.Antiguedad, NOW(), CURTIME(), CURRENT_USER());
+
+CREATE TRIGGER `tr_delete_tecnicos` 
+AFTER DELETE ON `tecnicos` 
+FOR EACH ROW 
+INSERT INTO `log_tecnicos` (Accion, Legajo_tec, Fecha, Hora, Usuario) 
+VALUES ('DELETE', OLD.Legajo_tec, NOW(), CURTIME(), CURRENT_USER());
+
+INSERT INTO `tecnicos` (`Legajo_tec`, `Nombre_tec`, `Apellido_tec`, `Categoria`, `Antiguedad`) 
+VALUES ('7', 'Julio', 'Freire', 'Medio Oficial', '1');
+
+UPDATE tecnicos SET Categoria = 'Oficial' WHERE Legajo_tec = 6;
+
+DELETE FROM tecnicos WHERE Legajo_tec = 7;
+
+SELECT * FROM tecnicos;
+SELECT * FROM log_tecnicos;
+
+
+
